01bbf2cd0b79ca0ee94c0a044a62d18fe93148a2
Ahmad Fauzan <fauzan18296@gmail.com>
2024-09-04 23:58:07 +0700

    add: merubah corsnya

--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({
-  origin: 'http://127.0.0.1:5173/',
-  methods: 'GET'
-}))
+app.use(cors())
 app.use('/image', routes)
 app.use(logsRequest)
 

8c1d82789cc2d4419dc1fcecffefe9fd6f3a67ce
Ahmad Fauzan <fauzan18296@gmail.com>
2024-09-04 21:56:38 +0700

    add: hanya menambahkan cors dan envnya saja

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes/image-menu.js';
 import logsRequest from './middleware/logs.js';
+import cors from 'cors'
 import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors())
 app.use('/image', routes)
 app.use(logsRequest)
 
